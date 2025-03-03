01c673c92621cac881d319332cddb16bd6d4c50b
cstone45 <cstone45@usf.edu>
2025-03-02 22:27:21 -0500

    Task 2 - Updated Metric Cards via Array Construction

--- cc_12.js
+++ cc_12.js
@@ -7,3 +7,8 @@ setAttribute(Id, "revenueCard");
 let title = "Revenue"
 let placeHolderValue = 0
 Revenue.appendChild(newMetric);
+
+//Task 2: Updating Dashboard Metrics - Working with NodeLists and Arrays
+const allMessages = document.querySelectorAll(`.metric-card`);
+const arrayOfNodes = Array.from(allMessages);
+allMessages.forEach(background-color: "Light-Green");
